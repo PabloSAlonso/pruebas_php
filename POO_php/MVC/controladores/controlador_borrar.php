6f14171bb6c9c7863b40480177b630a46401aed5
PabloSAlonso <santanaalonsopablo@gmail.com>
2025-11-07 11:59:03 +0100

    changes

--- POO_php/MVC/controladores/controlador_borrar.php
+++ POO_php/MVC/controladores/controlador_borrar.php
@@ -1,11 +1,8 @@
 <?php
-
     if (isset($_GET['id']) && is_numeric($_GET['id'])){
         require_once '../modelos/modelo.php';
 
-        
-
+        $empleado = new Empleado();
         
     }
-
 ?>
--- POO_php/MVC/modelos/modelo.php
+++ POO_php/MVC/modelos/modelo.php
@@ -1,6 +1,7 @@
 <?php
 
-class Empleado {
+class Empleado
+{
 
     private $empleado;
     private $db;
@@ -11,14 +12,16 @@ class Empleado {
         $this->db = new PDO("mysql:host=localhost;dbname=ejemplo_mvc;charset=utf8", "root", "");
     }
 
-    public function setEmpleado($nombre, $apellidos, $telefono, $departamento){
+    public function setEmpleado($nombre, $apellidos, $telefono, $departamento)
+    {
         $sql = "INSERT INTO empleados(nombre,apellidos,telefono,departamento) VALUES('$nombre','$apellidos','$telefono','$departamento')"; //devuelve true si se ejecuta bien
         $result = $this->db->query($sql);
         $this->db = null;
         return $result;
     }
 
-    public function getEmpleado(){
+    public function getEmpleado()
+    {
         $sql = "SELECT * FROM empleados";
         $result = $this->db->query($sql);
         $this->empleado = $result->fetchAll(PDO::FETCH_ASSOC);
@@ -26,12 +29,28 @@ class Empleado {
         return $this->empleado;
     }
 
-    public function editarEmpleado($id){
+    public function editarEmpleado($id)
+    {
         $sql = "SELECT nombre, apellidos, telefono, departamento From empleados WHERE id={$id}";
-        $result = $this -> db -> query($sql);
-        $dato = $result -> fetchAll(PDO::FETCH_ASSOC);
-        $this -> db = null;
+        $result = $this->db->query($sql);
+        $dato = $result->fetchAll(PDO::FETCH_ASSOC);
+        $this->db = null;
         return $dato;
+    }
 
+    public function actualizarEmpleado($id)
+    {
+        $sql = "UPDATE empleados SET nombre = {$_POST['nombre']} apellidos = {$_POST['apellidos']} telefono = {$_POST['telefono']} departamento = {$_POST['departamento']}";
+        $result = $this->db->query($sql);
+        $this->db = null;
+        return $result;
+    }
+
+    public function borrarEmpleado($id)
+    {
+        $sql = "DELETE FROM empleados WHERE id = {$id}";
+        $result = $this->db->query($sql);
+        $this->db = null;
+        return $result;
     }
 }
