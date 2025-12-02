c41af3d48064dbd06034b4a0380ecc9eb9d988c8
PabloSAlonso <santanaalonsopablo@gmail.com>
2025-12-02 11:20:07 +0100

    changes

---
+++ Ejercicio_mvc_libre/vistas/vista.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vista Principal</title>
+</head>
+<body>
+    <h1>Listado </h1>
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Nationality</th>
+            <th>Years Played</th>
+            <th>Nº Teams Played</th>
+            <th>All Star Game</th>
+            <th>MVP</th>
+        </tr>
+    </table>
+</body>
+</html>
